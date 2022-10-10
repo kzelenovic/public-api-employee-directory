53a8d5731be231e719aab5ea60f32e892057db6c
kzelenovic <106201960+kzelenovic@users.noreply.github.com>
2022-10-10 16:47:40 -0700

    use async/await for fetching and printing employee data

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,32 @@
-fetch('https://randomuser.me/api/?results=12&inc=picture,name,email,location,phone,dob&nat=us')
-    .then(res => res.json())
-    .then(data => generateCard(data.results))
-    .catch(err => console.log(err));
-
 const gallery = document.querySelector('#gallery');
 
-function generateCard(data) {
-    console.log(data)
-    console.log(data[0].name.first)
-    for (let i = 0; i < data.length; i++) {
-        const person = data[i];
+async function getEmployees(url) {
+    try{
+        const employeeResponse = await fetch(url);
+        const employeeJSON = await employeeResponse.json();
+        return employeeJSON.results;
+    } catch(error) {
+        throw error;
+    }
+}
+
+async function generateCards(url) {
+    const employeeArr = await getEmployees(url);
+
+    for (let i = 0; i < employeeArr.length; i++) {
+        const employee = employeeArr[i];
         gallery.insertAdjacentHTML('beforeend', `
         <div class="card">
             <div class="card-img-container">
-            <img class="card-img" src="${person.picture.large}" alt="profile picture">
+            <img class="card-img" src="${employee.picture.large}" alt="profile picture">
             </div>
             <div class="card-info-container">
-                <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
-                <p class="card-text">${person.email}</p>
-                <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
+                <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+                <p class="card-text">${employee.email}</p>
+                <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
             </div>
         </div>`)
     }
 }
+
+generateCards('https://randomuser.me/api/?results=12&inc=picture,name,email,location,phone,dob&nat=us');
